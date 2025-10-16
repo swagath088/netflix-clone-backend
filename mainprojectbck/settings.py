b548cdccb3a8f9c7678b7cd5801e0430fd86b722
swagath0 <sswagath46@gmail.com>
2025-10-16 14:49:58 +0530

    Added Postgres config and dependencies

--- mainprojectbck/settings.py
+++ mainprojectbck/settings.py
@@ -90,12 +90,22 @@ WSGI_APPLICATION = 'mainprojectbck.wsgi.application'
 #   }
 #}
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
+
 
 
 
