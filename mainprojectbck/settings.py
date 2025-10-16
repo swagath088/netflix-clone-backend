59357cb53710e6102881b12cd7ba870ee6d665f8
swagath0 <sswagath46@gmail.com>
2025-10-16 17:39:32 +0530

    Fix: ALLOWED_HOSTS to ['*'] for Render deployment

--- mainprojectbck/settings.py
+++ mainprojectbck/settings.py
@@ -14,9 +14,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # Allowed hosts
-ALLOWED_HOSTS = os.environ.get(
-    'ALLOWED_HOSTS', '127.0.0.1,localhost'
-).split(',')
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 INSTALLED_APPS = [
